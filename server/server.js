fb3fa38591269f09e4f54e8f122d9116e5e83e1f
KrishnaPalde <paldekrishna4325@gmail.com>
2024-09-01 06:00:05 +0530

    ENV file updated

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     // origin: "https://pk-1624-livid.vercel.app",
-    origin: "http://localhost:5173",
+    origin: process.env.ORIGIN,
     credentials: true,
   })
 );
